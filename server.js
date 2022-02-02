c6695470ac94039b882958f6d67ce37bcd198228
helloprof <suluxan.seneca@gmail.com>
2022-02-02 14:08:34 -0500

    WIP endpoints/routes - lyrics query by ID, about, music, 404

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 const path = require('path')
+const musicData = require('./musicData')
 
 const HTTP_PORT = process.env.port || 8080
 const onHttpStart = () => console.log(`HTTP server is listening on port ${HTTP_PORT} 🚀🚀🚀`)
@@ -13,9 +14,44 @@ app.get('/', (req, res) => {
 })
 
 app.get('/home', (req, res) => {
-    // res.send('hello home')
     res.sendFile(path.join(__dirname, '/views/index.html'))
+})
 
+app.get('/lyrics', (req, res) => {
+    // res.send('hello lyrics')
+    musicData.getAlbums().then((data) => {
+        res.json(data)
+    }).catch((error) => {
+        console.log(error)
+        res.status(404).send("ERROR!")
+    })
+})
+
+app.get('/lyrics/:id', (req, res) => {
+    // res.send('hello lyrics')
+    musicData.getAlbums().then((data) => {
+        // res.json(data)
+        // res.send(req.params.id)
+        // resolvedPromiseData[idFromRequestParams].field
+        res.json(data[req.params.id-1].lyrics)
+
+    }).catch((error) => {
+        console.log(error)
+        res.status(404).send("ERROR!")
+    })
+})
+
+
+app.get('/about', (req, res) => {
+    res.send('hello about')
+})
+
+app.get('/music', (req, res) => {
+    res.send('hello music')
+})
+
+app.use((req, res) => {
+    res.status(404).send("PAGE NOT FOUND!!")
 })
 
 app.listen(HTTP_PORT, onHttpStart)
