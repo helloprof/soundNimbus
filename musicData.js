a046caaa9a22893cc34499966669fb5f32a29826
helloprof <suluxan.seneca@gmail.com>
2022-03-09 15:51:00 -0500

    updated data from fs+array to postgres+sequelize
    initialize: readFile -> db sync
    
    getAlbums: albums array -> findAll in db
    
    addAlbum: push album into array -> create new album in db
    
    deleteAlbum: delete album using albumID from db
    
    WIP: Song table

--- musicData.js
+++ musicData.js
@@ -1,37 +1,117 @@
-const fs = require('fs')
+const env = require('dotenv')
+env.config()
 
 let albums = []
 
+const Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: 5432,
+    dialectOptions: {
+        ssl: { rejectUnauthorized: false }
+    },
+    query: { raw: true }
+});
+
+var Album = sequelize.define('Album', {
+    albumID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true 
+    },
+    title: Sequelize.STRING,
+    year: Sequelize.INTEGER,
+    artist: Sequelize.STRING,
+    imagePath: Sequelize.STRING
+})
+
+var Song = sequelize.define('Song', {
+    songID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true 
+    },
+    title: Sequelize.STRING,
+    musicPath: Sequelize.STRING,
+    lyrics: Sequelize.STRING
+})
+
+Album.hasMany(Song, {foreignKey: 'albumID'})
+
 module.exports.initialize = function() {
     return new Promise((resolve, reject) => {
-        fs.readFile('./data/albums.json', 'utf-8', (err, data) => {
-            if(err) {
-                reject(err)
-            } else {
-                albums = JSON.parse(data)
-                console.log(albums)
-
-                resolve()
-            }
+        sequelize.sync().then(() => {
+            resolve()
+        }).catch((error) => {
+            console.log(error)
+            reject("SYNC FAILED")
         })
-    } )
-}
+    })}
+
 
 module.exports.getAlbums = function() {
     return new Promise((resolve, reject) => {
-        if (albums.length > 0 ) {
-            resolve(albums) 
-        } else {
-            reject("no results returned"); 
-        }
+        Album.findAll().then((data) => {
+            resolve(data)
+        })
     })
 }
 
 
 module.exports.addAlbum = function(album) {
     return new Promise((resolve, reject) => {
-        album.id = albums.length + 1
-        albums.push(album)
-        resolve()
+        Album.create(album).then(() => {
+            console.log("ALBUM CREATED")
+    
+        //     Song.create({
+        //         title: "Paranoid",
+        //         musicPath: "/music/paranoid_kanye.mp3",
+        //         lyrics: "Why are you so paranoid",
+        //         albumID: album.albumID
+        //     })
+        //   }).then(() => console.log("SONG CREATED"))
+
+            resolve()
+
+        }).catch((error) => {
+            console.log("ALBUM ERROR:")
+            console.log(error)
+        })
+
+        // album.id = albums.length + 1
+        // albums.push(album)
+        // resolve()
+    })
+}
+
+module.exports.deleteAlbum = function(albumID) {
+    return new Promise((resolve, reject) => {
+        Album.destroy({
+            where: {
+                albumID: albumID
+            }
+        }).then(() => {
+            console.log("ALBUM DELETED")
+    
+        //     Song.create({
+        //         title: "Paranoid",
+        //         musicPath: "/music/paranoid_kanye.mp3",
+        //         lyrics: "Why are you so paranoid",
+        //         albumID: album.albumID
+        //     })
+        //   }).then(() => console.log("SONG CREATED"))
+
+            resolve()
+
+        }).catch((error) => {
+            console.log("ALBUM DELETE ERROR:")
+            console.log(error)
+        })
+
+        // album.id = albums.length + 1
+        // albums.push(album)
+        // resolve()
     })
 }
