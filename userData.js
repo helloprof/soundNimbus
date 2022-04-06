f870f5a518a796a07707340f9c567709226f6a0f
helloprof <suluxan.seneca@gmail.com>
2022-04-06 14:30:09 -0400

    verifyLogin function that checks for a specific user and checks their password and updates loginHistory, comments with encrypted password functioins

--- userData.js
+++ userData.js
@@ -36,35 +36,66 @@ module.exports.initialize = function () {
 
 module.exports.registerUser = function(userData) {
     return new Promise(function (resolve, reject) {
-
         if (userData.password === userData.password2) {
+            
+            // bcrypt.hash(userData.password, 10).then((hash) => {
+            // userData.password = hash   
+            // do all user stuff 
+            // })
             let newUser = new User(userData)
 
             newUser.save((err) => {
                 if(err) {
                   // there was an error
                   console.log(err);
+                  reject("Error creating new user:" + err)
                 } else {
                   // everything good
                   console.log(newUser);
                   resolve(userData)
-    
                 }
               });
-        }
- 
-
-        // if (user) {
-        //     console.log("USER DATA: ")
-        //     console.log(user)
-        //     resolve(user)
-        // } else {
-        //     reject()
-        // }
-        
+        } else {
+            reject("PASSWORDS DO NOT MATCH!!")
+        }    
     });
 };
 
+module.exports.verifyLogin = function(userData) {
+    return new Promise((resolve, reject) => {
+        User.findOne({ username: userData.username})
+        .exec()
+        .then((mongoData) => {
+
+            // bcrypt.compare(userData.password, mongoData.password).then((result) => {
+            //    if (result == true) {
+            //      // login successful stuff
+            //  }
+            // })
+            
+            if (userData.password === mongoData.password) {
+                console.log("SUCCESSFUL LOGIN!")
+
+                mongoData.loginHistory.push({dateTime: new Date(), userAgent: userData.userAgent})
+
+                User.updateOne( 
+                    { username: userData.username }, 
+                    { $set: {loginHistory: mongoData.loginHistory}}
+                ).exec()
+                .then(() => { 
+                    resolve(mongoData)
+                })
+
+            } else {
+                reject("LOGIN UNSUCCESSFUL - PASSWORD INCORRECT")
+            }   
+        })
+        .catch((error) => {
+            console.log(error)
+            reject("LOGIN UNSUCCESSFUL - ERROR:" + error)
+        })
+    })
+}
 
 
 
